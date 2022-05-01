baf4f6bc2f0d1805fb4b385a308a5dff2d880be4
Rutuja-Mangle-07 <69417610+Rutuja-Mangle-07@users.noreply.github.com>
2022-05-02 00:20:45 +0530

    Update fp_mac.v

--- fp_mac.v
+++ fp_mac.v
@@ -1,21 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.11.2018 14:33:51
-// Design Name: 
-// Module Name: fp_mac
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
+
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
